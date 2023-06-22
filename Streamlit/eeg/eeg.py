0ac2e8547fcb45fa028e2ff740f3d113278c4457
stevenmatthew31 <stevenmatthewgg@gmail.com>
2023-06-22 18:32:57 +0700

    restore

--- Streamlit/eeg/eeg.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-from .filter import lpf, hpf
-
-def eeg(signal, fs = 512):
-    signal = lpf(signal, 2*np.pi*35, fs)
-    signal = hpf(signal, 2*np.pi*1, fs)
-    return signal
--- Streamlit/eeg/tools.py
+++
@@ -1,6 +0,0 @@
-def get_index(data, index):
-    if index < 0:
-        return 0
-    if index > data.size - 1:
-        return 0
-    return data[index]
